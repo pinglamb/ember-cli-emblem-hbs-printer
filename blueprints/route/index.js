293fb3199ea862f4ebd309c60cec8788361d2996
jsec <trollface@jsec.me>
2015-04-05 23:59:10 -0400

    add blueprints for emblem templates

---
+++ blueprints/route/index.js
@@ -0,0 +1,45 @@
+module.exports = {
+  description: '',
+
+  availableOptions: [
+    {
+      name: 'type',
+      type: String,
+      values: ['route', 'resource'],
+      default: 'route',
+      aliases:[
+        {'route': 'route'},
+        {'resource': 'resource'}
+      ]
+    },
+    {
+      name: 'path',
+      type: String,
+      default: ''
+    }
+  ],
+
+  fileMapTokens: function() {
+    return this.lookupBlueprint('route').fileMapTokens();
+  },
+
+  beforeInstall: function(options) {
+    return this.lookupBlueprint('route').beforeInstall(options);
+  },
+
+  shouldTouchRouter: function(name) {
+    return this.lookupBlueprint('route').shouldTouchRouter(name);
+  },
+
+  afterInstall: function(options) {
+    return this.lookupBlueprint('route').afterInstall(options);
+  },
+
+  beforeUninstall: function(options) {
+    return this.lookupBlueprint('route').beforeUninstall(options);
+  },
+
+  afterUninstall: function(options) {
+    return this.lookupBlueprint('route').afterUninstall(options);
+  }
+};
