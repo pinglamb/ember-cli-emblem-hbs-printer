011ea5c02dbad627a81cf3ce212b53163107a74c
pinglamb <pinglambs@gmail.com>
2015-04-09 02:17:26 +0800

    Just generate template

--- blueprints/component/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  description: 'Generates a component. Name must contain a hyphen.',
-  locals: function(options) {
-    return this.lookupBlueprint('component').locals(options);
-  },
-  fileMapTokens: function() {
-    return this.lookupBlueprint('component').fileMapTokens();
-  }
-};
